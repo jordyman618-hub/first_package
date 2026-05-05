23b516fe228b70d7bb2101c6a198d85d0ec9f049
jordyman618-hub <jordyman618@gmail.com>
2026-05-05 15:07:07 +0200

    Try_1.01

---
+++ Configuration/Backend/BackendLayouts.php
@@ -0,0 +1,16 @@
+'config' => [
+    'backend_layout' => [
+        'colCount' => 2,
+        'rowCount' => 1,
+        'nodes' => [
+            '0-0' => [
+                'name' => 'c1 (50%)',
+                'colPos' => 101,
+                ],
+                '0-1' => [
+                'name' => 'c2 (50%)',
+                'colPos' => 102,
+                ],
+            ],
+        ],
+    ]
