67d6afc9a9505a6457af547814947bcbfa8b7721
jordyman618-hub <jordyman618@gmail.com>
2026-05-05 15:54:28 +0200

    try_1.02

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,8 @@
+<?php
+define('TYPO3') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'first_package',
+    'Configuration/TypoScript',
+    'Local Grids Configuration'
+);
