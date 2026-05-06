ccab01bc0900a005bb50199a7dbf4eead694c20e
jordyman618-hub <jordyman618@gmail.com>
2026-05-06 14:01:07 +0200

    ...

--- Classes/Controller/DefaultController.php
+++ Classes/Controller/DefaultController.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Vendor\FirstPackage\controller;
+namespace Vendor\FirstPackage\Controller;
 
 use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
-class DefaultControllerextends ActionController{
+class DefaultController extends ActionController{
 
 public function containerAction() : ResponseInterface{
     return $this -> htmlResponse();
